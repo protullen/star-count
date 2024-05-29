1ce4762e98d48aa67b939ad97ee243681a294be6
Protullen <138602811+protullen@users.noreply.github.com>
2024-05-29 17:45:31 +0530

    Create star-count.js

---
+++ star-count.js
@@ -0,0 +1,21 @@
+// MongoDB connection setup
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://wroxen240:<password>@cluster0.zdudrwn.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+export default async function handler(req, res) {
+  try {
+    if (!client.isConnected()) {
+      await client.connect();
+    }
+
+    const db = client.db('myDatabase');
+    const collection = db.collection('stars');
+    const count = await collection.countDocuments();
+
+    res.status(200).json({ count });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
